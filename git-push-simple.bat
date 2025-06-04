014e355c8fcf5aa85938d2b084bf1cd06833c34f
Neonova UI <2miguelones@gmail.com>
2025-06-04 09:54:26 -0500

    Actualizaci├│n autom├ítica

--- git-push-simple.bat
+++ git-push-simple.bat
@@ -6,12 +6,8 @@ echo Agregando archivos...
 git add .
 echo.
 
-echo Ingrese el mensaje del commit:
-set /p commit_message=
-
-echo.
-echo Creando commit con mensaje: %commit_message%
-git commit -m "%commit_message%"
+echo Creando commit...
+git commit -m "Actualización automática"
 echo.
 
 echo Subiendo cambios al repositorio...
